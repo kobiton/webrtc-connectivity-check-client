d79f971361c02987a3441e73b6a54560d026c9bb
Trương Lê Việt Danh <tlvietdanh@gmail.com>
2022-03-23 15:42:22 +0700

    Complete the first version of the check tool (#1)

---
+++ apps/client/README.md
@@ -0,0 +1,36 @@
+A client app running at customer's workstation to diagnose common network issues which causes the Lightning mode not running on Kobiton devices
+
+# 1. Setup
+
+- Download and install [NodeJS LTS](https://nodejs.org/en/download/) accordingly with your OS. If you already have NodeJS installed, make sure your NodeJS's version is 8.0 or newer
+- Download the [apps/client/index.js](https://raw.githubusercontent.com/kobiton/webrtc-connectivity-check/main/apps/client/index.js) to your machine. Let's say it's downloaded at `/tmp/` (on MacOS) or `C:/tmp/` (on Windows)
+
+# 2. Run the client app
+
+> Make sure your machine stays inside the organization network i.e. in the office so that the client app can report correct network condition
+
+- Open the Terminal app (on MacOS) or Command Prompt (on Windows)
+- Navigate to the directory having the `index.js` as below
+
+```
+cd /tmp (on MacOS)
+C:/tmp (on Windows)
+```
+
+- Execute the client app as below
+
+```
+node index.js
+```
+
+- Let it running, observe the result and take the suggested action
+
+# 3. Advanced run
+
+> This section is for Kobiton Technical Support staff
+
+The app supports below parameters (with default values) so that we can override on executing if needed
+
+- `SERVER_URL=http://13.213.3.169` : specify the server IP i.e. the `apps/server` runs there
+- `CLIENT_UDP_PORT=41234` : specifiy the port (in UDP) that the server app is listening
+
---
+++ apps/client/index.js
@@ -0,0 +1,133 @@
+const http = require('http');
+const udp = require('dgram');
+
+const SERVER_URL = process.env.SERVER_URL || 'http://13.213.3.169'
+const CLIENT_UDP_PORT = Number(process.env.CLIENT_UDP_PORT) || 41234
+const MESSAGE_RESPONSE_TIMEOUT_IN_MS = 5000
+
+let udpClient = null
+const currentDate = new Date()
+let timeout = null
+let sentMessage = ''
+
+function printLogs(message) {
+    console.log(`[${currentDate.toString()}] [LOG]`, message);
+}
+
+function printError(message, err) {
+    console.error(`[${currentDate.toString()}] [ERROR]`, message, err.message ? err.message : err, err);
+}
+
+function createUdpServer() {
+    udpClient = udp.createSocket('udp4')
+
+    udpClient.on('error', (error) => {
+        printError('Something wrong when creating udp server. Please re-run the client or inform with customer-support. Error message: ', error)
+        udpClient.close()
+        process.exit(-1)
+    })
+
+    udpClient.on('message', handleReceivedMessageFromServer)
+
+    udpClient.on('listening', () => {
+        const address = udpClient.address()
+        printLogs(`Started UDP client at port ${address.port}`)
+        beginConnectivityCheck()
+
+    });
+
+    udpClient.on('close', () => {
+        printLogs('The UDP server is closed!');
+    });
+
+    udpClient.bind(CLIENT_UDP_PORT)
+}
+
+function getRequest(url) {
+    return new Promise ((resolve, reject) => {
+      http.get(url, (res) => {
+        const { statusCode } = res;
+        let data = ''
+        res.on('data', d => {data += d})
+        res.on('close', () => {resolve({data, statusCode})})
+        res.on('error', (error) => {reject(error)})
+      })
+    });
+}
+
+
+async function beginConnectivityCheck() {
+    // Get server information (udp port, udp address)
+    let serverAddress
+    const url = `${SERVER_URL}/address`
+    try {
+        printLogs(`Calling ${url} to retrieve the Kobiton UDP server address`)
+        const response = await getRequest(url)
+        serverAddress = JSON.parse(response.data)
+        printLogs(`The Kobiton UDP server is at ${serverAddress.address}:${serverAddress.port}`)
+    } catch (error) {
+        printError('Unexpected error when getting server address, please solve and try again', error)
+        process.exit(-1)
+    }
+    
+    // Sending timestamp data to server and wait for response
+    sentMessage = currentDate.getTime().toString()
+    const data = Buffer.from(sentMessage)
+    printLogs('[CHECK 1] Can the client send UDP packets to the Internet ?')
+    printLogs(`The client is going to send an UDP packet to Kobiton UDP server at ${serverAddress.address}:${serverAddress.port}`)
+    udpClient.send(data, Number(serverAddress.port), serverAddress.address, (error) => {
+        if (error){
+            printError('Unexpected error occurs when sending data. Please solve and try again' , error);
+            process.exit(-1)
+        }
+    });
+    setTimeout(async () => {
+        // Call api to verify that server received message successfully.
+        const response = await getRequest(`${SERVER_URL}/message?content=${sentMessage}`)
+        if (response.statusCode === 200) {
+            printLogs('[CHECK 1 - PASSED] The client can send the UDP packet successfully to the Internet since the Kobiton UDP server receives the packet')
+        }
+        else if (response.statusCode === 404) {
+            printLogs('[CHECK 1 - FAILED] The client can NOT send the UDP packet to the Internet. Please verify with your IT departmant that: in the office, the outgoing UDP traffic to the Internet with destination port range 30000-65000 is enabled on the router and/or the firewall.')
+        }
+        
+        printLogs('[CHECK 2] Can the client receive UDP packets from the Internet (sent by Kobiton UDP server) ?')
+
+        // Wait for server response.
+        timeout = setTimeout(() => {
+            printLogs('[CHECK 2 - FAILED] The client can NOT receive the UDP packet from the Internet. Please verify with your IT departmant that: in the office, the incoming UDP traffic from the Internet with source port range 30000-65000 is enabled on the router and/or the firewall.')
+            bye()
+        }, MESSAGE_RESPONSE_TIMEOUT_IN_MS);
+    }, 1000);
+
+}
+
+function handleReceivedMessageFromServer() {
+    if (timeout) {
+        clearTimeout(timeout)
+        timeout = null
+    }
+
+    printLogs('[CHECK 2 - PASSED] The client receives an UDP packet from Kobiton UDP server.')
+    bye()
+}
+
+function bye() {
+    printLogs('')
+    printLogs("The check process completes. If you encounter any issue, please follow the suggestion, solve it and re-run again to verify it's fixed")
+    process.exit(0)
+}
+
+function main() {
+    // Print envs
+    let msg = 'The client is launched with below environment variables\n'
+    msg += `\t- SERVER_URL=${SERVER_URL}\n`
+    msg += `\t- CLIENT_UDP_PORT=${CLIENT_UDP_PORT}`
+    printLogs(msg)
+
+    // Create udp client.
+    createUdpServer()
+}
+
+main()
+
